5a23062641ff49860f73dd6b88fb6cf1985056be
Osman <osmancansiz@github.com>
2019-11-21 00:26:32 +0300

    /DELETE:director_id Movies

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -115,6 +115,22 @@ describe("/api/movies tests", () => {
         });
     });
   });
+
+  describe("/DELETE:director_id Movies", () => {
+    it("it should DELETE a movie given by id", done => {
+      chai
+        .request(server)
+        .delete("/api/movies/"+ movieId)
+        .set("x-access-token", token)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("status").eql(1);
+          done();
+        });
+    });
+  });
+
 });
 
 //Maçın son kornerinde karşı kaleye koşan kaleci çaresizliğiyle geliyorum sana,
