468aa700b1abb9a3223eb9696dbe5ec6d25a55d2
Osman <osmancansiz@github.com>
2019-12-01 20:50:03 +0300

    db edit connection string again :(

--- helper/db.js
+++ helper/db.js
@@ -15,7 +15,3 @@ module.exports = () => {
   });
   mongoose.Promise = global.Promise;
 };
-
-// mongodb://ocansiz:osman123@cluster0-shard-00-00-tsiwk.mongodb.net:27017,cluster0-shard-00-01-tsiwk.mongodb.net:27017,cluster0-shard-00-02-tsiwk.mongodb.net:27017/movie-api-db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
-
-//mongodb+srv://ocansiz:osman123@cluster0-tsiwk.mongodb.net/test?retryWrites=true&w=majority
